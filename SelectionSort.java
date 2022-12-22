fbabcf9b20d8592f361ec9236c42f11e40281696
Quynh <hoaly2710@gmail.com>
2022-12-22 22:14:30 +0100

    added selectionsort

--- SelectionSort.java
+++ SelectionSort.java
@@ -6,4 +6,39 @@ public class SelectionSort {
 
 	}
 
+	public void sort1(int[] array) {
+		int marker = array.length - 1;
+		while (marker > 0) {
+			int index_max = 0;
+			for (int i = 1; i <= marker; i++) {
+				if (array[i] > array[index_max]) {
+					index_max = i;
+				}
+			}
+			this.swap(array, marker, index_max);
+			marker--;
+		}
+	}
+
+	public void swap(int[] a, int indx1, int indx2) {
+		int tmp = a[indx1];
+		a[indx1] = a[indx2];
+		a[indx2] = tmp;
+	}
+
+	public void sort2(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			int index_min = i;
+
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[j] < array[index_min]) {
+					index_min = j;
+				}
+			}
+			if (index_min != i) {
+				this.swap(array, i, index_min);
+			}
+		}
+	}
+
 }
